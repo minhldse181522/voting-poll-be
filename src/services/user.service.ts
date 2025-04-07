dc37a911c371217189a6ef2905dd8cbfd1f24871
minhle <minhldse181522@fpt.edu.vn>
2025-04-07 19:52:55 +0700

    feat: refactor user controller and service to use class-based structure; add login functionality and Swagger documentation

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,30 +1,45 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import prisma from "../prisma";
 
-export async function getUserService() {
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      username: true,
-    },
-  });
-  return users.map((user) => ({
-    user_id: user.id.toString(),
-    username: user.username,
-  }));
-}
-
-export async function getUserByIdService(id: number) {
-  const user = await prisma.user.findUnique({
-    where: { id: BigInt(id) },
-  });
-
-  if (!user) {
-    throw new Error("User not found");
+export class UserService {
+  static async getUserService() {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+      },
+    });
+    return users.map((user) => ({
+      user_id: user.id.toString(),
+      username: user.username,
+    }));
   }
 
-  return {
-    id: user.id.toString(),
-    username: user.username,
-  };
+  static async getUserByIdService(id: number) {
+    const user = await prisma.user.findUnique({
+      where: { id: BigInt(id) },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return {
+      id: user.id.toString(),
+      username: user.username,
+    };
+  }
+
+  static async loginUser(username: string, password: string) {
+    const user = await prisma.user.findUnique({
+      where: { username, password },
+    });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return {
+      id: user.id.toString(),
+      username: user.username,
+    };
+  }
 }
