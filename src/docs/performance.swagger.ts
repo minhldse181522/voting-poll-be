be24aaae9dcb15dfc161f047f71a8f90d48fcdf6
minhle <minhldse181522@fpt.edu.vn>
2025-04-08 13:15:31 +0700

    feat: implement create, update, and delete functionality for performances; update Swagger documentation

--- src/docs/performance.swagger.ts
+++ src/docs/performance.swagger.ts
@@ -82,7 +82,7 @@
  * @swagger
  * /api/performances/{id}:
  *   put:
- *     summary: Update a performance by ID
+ *     summary: Update vote performance by ID
  *     tags:
  *       - Performance
  *     parameters:
@@ -111,4 +111,130 @@
  *         description: Unauthorized
  *       404:
  *         description: Performance not found
+ */
+
+/**
+ * @swagger
+ * /api/performance:
+ *   post:
+ *     summary: Create a new performacne
+ *     tags:
+ *       - Performance
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the performance
+ *                 example: "Một triệu lý do"
+ *               bgDesktop:
+ *                 type: string
+ *                 description: Background of Desktop (có thể để trống)
+ *                 example: "https:..."
+ *               bgPhone:
+ *                 type: string
+ *                 description: Background of Phone (có thể để trống)
+ *                 example: "https:..."
+ *     responses:
+ *       201:
+ *         description: Performance created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The performance ID
+ *                   example: 1
+ *               name:
+ *                 type: string
+ *                 description: The name of the performance
+ *                 example: "Một triệu lý do"
+ *               bgDesktop:
+ *                 type: string
+ *                 description: Background of Desktop (có thể để trống)
+ *                 example: "https:..."
+ *               bgPhone:
+ *                 type: string
+ *                 description: Background of Phone (có thể để trống)
+ *                 example: "https:..."
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/performance/{id}:
+ *   put:
+ *     summary: Update a performance by ID
+ *     tags:
+ *       - Performance
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The performance ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Team 5"
+ *               bgDesktop:
+ *                 type: string
+ *                 example: "https://..."
+ *               bgPhone:
+ *                 type: string
+ *                 example: "https://..."
+ *     responses:
+ *       200:
+ *         description: Performance updated successfully
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Performance not found
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/performance/{id}:
+ *   delete:
+ *     summary: Delete a performance by ID
+ *     tags:
+ *       - Performance
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The performance ID
+ *     responses:
+ *       200:
+ *         description: Performance deleted successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal server error
  */
--- src/routes/performance.routes.ts
+++ src/routes/performance.routes.ts
@@ -6,5 +6,8 @@ const router = Router();
 router.get("/performances", PerformanceController.getAllPerformances);
 router.get("/performanceByCategory/:id", PerformanceController.getAllPerformanceCategories);
 router.put("/performances/:id", PerformanceController.updateVotePerformance);
+router.post("/performance", PerformanceController.createPerformance)
+router.put("/performance/:id", PerformanceController.updatePerformance)
+router.delete("/performance/:id", PerformanceController.deletePerformance)
 
 export default router;
